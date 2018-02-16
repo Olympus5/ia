c9284e01baf789ca6371ee38f13d4e675c5883d2
Erwan IQUEL <erwan.iquel@gmail.com>
2018-02-16 17:49:04 +0100

    TP2 IA - fonction jouer ok

---
+++ tp2/Puissance4/Puissance4.java
@@ -0,0 +1,90 @@
+/*
+ * TP 3 : Puissance 4
+ * 
+ * @author Tassadit BOUADI.
+ */
+
+/**
+ * Programme principal du jeu du puissance 4.
+ * 
+ */
+public class Puissance4 {
+
+	public static void main(String[] args) {
+		//cr�ation des joueurs et appel de la fonction jouer
+		JoueurHumain joueur1 = new JoueurHumain();
+		JoueurAleatoire joueur2 = new JoueurAleatoire();
+		
+		jouer(joueur1, joueur2);
+	}
+	
+	
+	/**
+	 * Fonction qui effectue la boucle de jeu.
+	 * 
+	 * @param joueur1 : le premier joueur.
+	 * @param joueur2 : le second joueur.
+	 */
+	public static void jouer(Joueur joueur1, Joueur joueur2){
+		Resultat res = null;
+		int coup;
+		Grille grille = new Grille();
+		
+		Joueur joueurCour = joueur1;	
+		int numJoueur = Grille.JOUEUR1; //le joueur 1 commence � jouer
+		
+		int vainqueur = 0;//match nul
+		boolean jeuFini = false;
+		
+		
+		//boucle de jeu
+		while(!jeuFini || grille.estPleine()){
+			//affichage de la grille 
+			System.out.println(grille);
+			
+			// Les joueurs jouent
+			if(numJoueur == Grille.JOUEUR1) {
+				res = joueur1.coup(grille, numJoueur);
+			} else if(numJoueur == Grille.JOUEUR2) {
+				res = joueur2.coup(grille, numJoueur);
+			} else {
+				System.err.println("Error");
+				System.exit(-1);
+			}
+			
+			//On test si on a un coups gagnant
+			if(grille.coupGagnant(numJoueur, res.getColonne())) {
+				jeuFini = true;
+				vainqueur = numJoueur;
+			}
+			
+			// On joue notre coups
+			if(res != null) {
+				grille.joueEn(numJoueur, res.getColonne());
+			} else {
+				System.err.print("Error");
+				System.exit(-1);
+			}
+			
+			// Fin boucle
+			numJoueur = Grille.joueurSuivant(numJoueur);
+		}
+		
+		//affichage de la grille 
+		System.out.println(grille);
+		
+		
+		//affichage du vainqueur
+		switch(vainqueur){
+			case Grille.JOUEUR1:
+				System.out.println("Victoire du joueur 1");
+				break;
+			case Grille.JOUEUR2:
+				System.out.println("Victoire du joueur 2");
+				break;
+			default:
+				System.out.println("Match nul");
+		}
+	}
+
+}
