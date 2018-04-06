a957101d582824ce1af1be06c7db815165fc7000
Erwan IQUEL <erwan.iquel@gmail.com>
2018-04-06 17:03:44 +0200

    Init TP4

---
+++ tp2_fork/Puissance4/rendu/JoueurMinMax.java
@@ -0,0 +1,78 @@
+
+public class JoueurMinMax implements Joueur {
+	private static final int PROFONDEUR = 4;
+	private int it = 0;
+
+	@Override
+	public Resultat coup(Grille grille, int joueur) {
+		return max(grille.copie(), PROFONDEUR, joueur, -1);
+	}
+	
+	public Resultat max(Grille grille, int profondeur, int joueur, int colonne) {
+		Resultat tmp, res = null;
+		double eval = -(Double.MAX_VALUE);
+		
+		if(colonne > -1 && (profondeur == 0 || grille.estPleine() || grille.coupGagnant(joueur, colonne))) {
+			return new Resultat(colonne, (new FonctionEvaluationEleve()).evaluation(grille, joueur));			
+		} else {
+			int[] coups = grille.generateurCoups();
+			Grille rollback = grille.copie();
+			
+			for(int i = 0, c = coups.length; i < c; i++) {
+				System.out.println("Min-Max itération: :" + it++);
+				
+				if(profondeur != PROFONDEUR) {
+					grille.joueEn(joueur, coups[i]);
+				}
+				
+				if(profondeur == PROFONDEUR && coups[i] == 5) {
+					System.out.println(grille.coupGagnant(joueur, coups[i]));
+					System.out.println(grille);
+				}
+				
+				tmp = min(grille.copie(), profondeur-1, Grille.joueurSuivant(joueur), coups[i]);
+				
+				if(tmp.getColonne() == 4) {
+				}
+				
+				if(eval < tmp.getValeur()) {
+					eval = tmp.getValeur();
+					res = tmp;
+				}
+				
+				grille = rollback.copie();
+			}
+		}
+		
+		return res;
+	}
+	
+	public Resultat min(Grille grille, int profondeur, int joueur, int colonne) {
+		Resultat tmp, res = null;
+		double eval = Double.MAX_VALUE;
+		
+		if(colonne > -1 && (profondeur == 0 || grille.estPleine() || grille.coupGagnant(joueur, colonne))) {
+			return new Resultat(colonne, (new FonctionEvaluationEleve()).evaluation(grille, joueur));
+		} else {
+			int[] coups = grille.generateurCoups();
+			Grille rollback = grille.copie();
+			
+			for(int i = 0, c = coups.length; i < c; i++) {
+				System.out.println("Min-Max itération: :" + it++);
+
+				if(profondeur != PROFONDEUR) grille.joueEn(joueur, coups[i]);
+				tmp = max(grille.copie(), profondeur-1, Grille.joueurSuivant(joueur), coups[i]);
+				
+				if(eval > tmp.getValeur()) {
+					eval = tmp.getValeur();
+					res = tmp;
+				}
+				
+				grille = rollback.copie();
+			}
+		}
+		
+		return res;
+	}
+	
+}
---
+++ tp2_fork/rendu/Puissance4.java
@@ -0,0 +1,85 @@
+/*
+ * TP 3 : Puissance 4
+ * 
+ * @author Tassadit BOUADI.
+ */
+
+/**
+ * Programme principal du jeu du puissance 4.
+ * 
+ */
+public class Puissance4 {
+
+	public static void main(String[] args) {
+		//cr�ation des joueurs et appel de la fonction jouer
+		JoueurMinMaxProf joueur2 = new JoueurMinMaxProf();
+		JoueurHumain joueur1 = new JoueurHumain();
+		
+		jouer(joueur1, joueur2);
+	}
+	
+	
+	/**
+	 * Fonction qui effectue la boucle de jeu.
+	 * 
+	 * @param joueur1 : le premier joueur.
+	 * @param joueur2 : le second joueur.
+	 */
+	public static void jouer(Joueur joueur1, Joueur joueur2){
+		Resultat res = null;
+		int coup;
+		Grille grille = new Grille();
+		
+		Joueur joueurCour = joueur1;	
+		int numJoueur = Grille.JOUEUR1; //le joueur 1 commence � jouer
+		
+		int vainqueur = 0;//match nul
+		boolean jeuFini = false;
+		
+		
+		//boucle de jeu
+		while(!jeuFini && !grille.estPleine()){
+			//affichage de la grille 
+			System.out.println(grille);
+			
+			// Les joueurs jouent
+			if(numJoueur == Grille.JOUEUR1) {
+				res = joueur1.coup(grille, numJoueur);
+			} else if(numJoueur == Grille.JOUEUR2) {
+				res = joueur2.coup(grille, numJoueur);
+			} else {
+				System.err.println("Error");
+				System.exit(-1);
+			}
+			
+			//On test si on a un coups gagnant
+			if(grille.coupGagnant(numJoueur, res.getColonne())) {
+				jeuFini = true;
+				System.out.println(grille.coupGagnant(numJoueur, res.getColonne()));
+				vainqueur = numJoueur;
+			}
+			
+			grille.joueEn(numJoueur, res.getColonne());
+			
+			// Fin boucle
+			numJoueur = Grille.joueurSuivant(numJoueur);
+		}
+		
+		//affichage de la grille 
+		System.out.println(grille);
+		
+		
+		//affichage du vainqueur
+		switch(vainqueur){
+			case Grille.JOUEUR1:
+				System.out.println("Victoire du joueur 1");
+				break;
+			case Grille.JOUEUR2:
+				System.out.println("Victoire du joueur 2");
+				break;
+			default:
+				System.out.println("Match nul");
+		}
+	}
+
+}
